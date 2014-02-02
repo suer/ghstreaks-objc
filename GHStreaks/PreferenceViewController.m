e8e1d6086953b423573ec433f2e1c846fa823c4f
suer <suetsugu.r@gmail.com>
2014-02-02 10:51:55 +0900

    Added: preference

--- GHStreaks/PreferenceViewController.m
+++ GHStreaks/PreferenceViewController.m
@@ -14,6 +14,8 @@
 
 @property (strong, nonatomic) UILabel *deviceTokenLabel;
 
+@property (strong, nonatomic) UITextField *userNameTextField;
+
 @end
 
 @implementation PreferenceViewController
@@ -31,41 +33,51 @@
 {
     [super viewDidLoad];
     
+    [self addDeviceTokenLabel];
+    [self addUserNameTextField];
+    [self addRegisterButton];
+}
+
+- (void)addDeviceTokenLabel
+{
     AppDelegate *delegate = [AppDelegate sharedDelegate];
     self.deviceTokenLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 40, 300, 100)];
     const unsigned *tokenBytes = [[delegate getDeviceToken] bytes];
-    NSString *token = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
-                            ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
-                            ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
-                            ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
-    
+    NSString *token = @"";
+    if (tokenBytes != NULL) {
+        [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                       ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
+                       ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
+                       ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
+    }
     self.deviceTokenLabel.text = token;
-
+    
     [self.deviceTokenLabel setLineBreakMode:NSLineBreakByWordWrapping];
     [self.view addSubview:self.deviceTokenLabel];
-    
-    UIButton *sendingMailDeviceTokenButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    sendingMailDeviceTokenButton.frame = CGRectMake(10, 100, 300, 30);
-    [sendingMailDeviceTokenButton setTitle:@"デバイストークンをメールで送信" forState:UIControlStateNormal];
-    [sendingMailDeviceTokenButton addTarget:self action:@selector(sendingMailDeviceTokenButtonTapped:)forControlEvents:UIControlEventTouchDown];
-    [self.view addSubview:sendingMailDeviceTokenButton];
 }
 
--(void)sendingMailDeviceTokenButtonTapped:(UIButton *)button
+- (void)addUserNameTextField
 {
-    if([MFMailComposeViewController canSendMail] == NO)
-    {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"エラー" message:@"メール送信の設定されていません。操作を中止します。" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [alert show];
-        return;
-    }
-    MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
-    [mailViewController setSubject:@"デバイストークン"];
-    [mailViewController setMessageBody:self.deviceTokenLabel.text isHTML:false];
-    mailViewController.mailComposeDelegate = self;
+    self.userNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(10, 80, 300, 100)];
+    self.userNameTextField.placeholder = @"GitHub User Name";
+    [self.view addSubview:self.userNameTextField];
+}
+
+
+
+- (void)addRegisterButton
+{
+    UIButton *registerButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    registerButton.frame = CGRectMake(10, 200, 300, 30);
+    [registerButton setTitle:@"登録する" forState:UIControlStateNormal];
+    [registerButton addTarget:self action:@selector(registerButtonTapped:)forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:registerButton];
+}
+
+- (void)registerButtonTapped:(id)sender
+{
     
-    [self presentViewController:mailViewController animated:true completion:nil];
-    }
+}
 
 - ( void )mailComposeController:( MFMailComposeViewController* )controller didFinishWithResult:( MFMailComposeResult )result error:( NSError* )error
 {
